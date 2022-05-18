46726a3442b7539f226dbf3ca301a28c1d7bfd40
Nick the Community Scientist <nlintas.business@gmail.com>
2022-05-18 20:06:41 +0300

    Switch to Byte Arrays
    - add auto reload

--- src/main/java/eu/york/cloud_computing/file_conversion_service/microservices/text_to_pdf/controllers/TextToPdfController.java
+++ src/main/java/eu/york/cloud_computing/file_conversion_service/microservices/text_to_pdf/controllers/TextToPdfController.java
@@ -1,25 +1,21 @@
 package eu.york.cloud_computing.file_conversion_service.microservices.text_to_pdf.controllers;
 
-import com.lowagie.text.Document;
+import com.itextpdf.kernel.pdf.PdfDocument;
 import eu.york.cloud_computing.file_conversion_service.microservices.text_to_pdf.services.TextToPDFService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.logging.Logger;
 
-import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
-
 @RestController
 // Not to be used individually, should be accessed by the web server.
 public class TextToPdfController {
     // Attributes
-    protected Logger logger = Logger.getLogger(TextToPdfController.class.getName());
     protected TextToPDFService textToPDFService;
 
     // Constructor
@@ -29,17 +25,20 @@ public class TextToPdfController {
 
     // Routes
     @RequestMapping("/c2f")
-    public void doC2F(HttpServletResponse response) throws IOException {
+    public ResponseEntity<byte[]> doC2F() {
         // Create pdf name
         DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy:hh:mm:ss");
         String currentDateTime = dateFormatter.format(new Date());
-        // Set Headers
-        response.setContentType("application/pdf");
+        // Prepare Headers
         String headerKey = "Content-Disposition";
         String headerValue = "inline; filename=pdf_" + currentDateTime + ".pdf";
-        response.setHeader(headerKey, headerValue);
-        // Return
-        this.textToPDFService.c2f(response);
+        // Get converted result
+        byte[] res = this.textToPDFService.c2f();
+        // Send a response
+        return ResponseEntity.ok()
+                .header(headerKey, headerValue)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(res);
     }
 
 }
--- src/main/java/eu/york/cloud_computing/file_conversion_service/microservices/text_to_pdf/services/TextToPDFService.java
+++ src/main/java/eu/york/cloud_computing/file_conversion_service/microservices/text_to_pdf/services/TextToPDFService.java
@@ -1,31 +1,32 @@
 package eu.york.cloud_computing.file_conversion_service.microservices.text_to_pdf.services;
 
-import com.lowagie.text.Document;
-import com.lowagie.text.PageSize;
-import com.lowagie.text.Paragraph;
-import com.lowagie.text.pdf.PdfWriter;
+import com.itextpdf.io.source.ByteArrayOutputStream;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
 import org.springframework.stereotype.Service;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 // Responsible for managing the conversion process of text to pdf's
 @Service
 public class TextToPDFService {
     // Methods
-    public void c2f(HttpServletResponse response) throws IOException {
+    public byte[] c2f() {
         // Create Pdf
-        Document document = new Document(PageSize.A4);
-        PdfWriter.getInstance(document, response.getOutputStream());
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        var writer = new PdfWriter(output);
+        var pdf = new PdfDocument(writer);
+        var document = new Document(pdf);
+        // Format
         format(document);
+        // Finalize Operation
+        document.close();
+        return output.toByteArray();
     }
 
     // All pdf formatting should occur within. The document is preserved without a return.
     private void format(Document document) {
-        document.open();
-        Paragraph paragraph2 = new Paragraph("test");
-        document.add(paragraph2);
-        document.close();
+        document.add(new Paragraph("Test"));
     }
 }
 
